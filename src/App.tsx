9792f42087f64ee52e99ab5e2d5392efeb9bd4b2
zackcole <183650880+zackcole@users.noreply.github.com>
2026-06-23 15:27:16 +0500

    update:privacy policy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Switch, Route } from "wouter";
+// import { Switch, Route } from "wouter";
+import { BrowserRouter as BrowsRouter, Routes, Route } from "react-router-dom";
+
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -11,14 +13,16 @@ import TermsConditions from "@/pages/TermsConditions";
 
 function Router() {
   return (
-    <Switch>
-      {/* Add pages below */}
-      <Route path="/" component={Bru} />
-      <Route path="/privacy-policy" component={PrivacyPolicy} />
-      <Route path="/terms-conditions" component={TermsConditions} />
-      {/* Fallback to 404 */}
-      <Route component={NotFound} />
-    </Switch>
+    <BrowsRouter>
+      <Routes>
+        {/* Add pages below */}
+        <Route path="/" element={<Bru />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-conditions" element={<TermsConditions />} />
+        {/* Fallback to 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowsRouter>
   );
 }
 
